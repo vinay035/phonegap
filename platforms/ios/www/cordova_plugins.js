50e3aec2f033b1e9a38ea532e210233d353682bd
Vinay Agarwal <vinay035@gmail.com>
2014-05-11 01:43:21 +0530

    initial commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,64 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "window.facebookConnectPlugin"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.oauthio/www/oauth.js",
+        "id": "com.phonegap.plugins.oauthio.OAuth",
+        "clobbers": [
+            "OAuth"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugin.statusbar/www/statusbar.js",
+        "id": "com.phonegap.plugin.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.facebookconnect": "0.5.0",
+    "com.phonegap.plugins.oauthio": "0.1.1",
+    "org.apache.cordova.device": "0.2.10-dev",
+    "org.apache.cordova.console": "0.2.8",
+    "com.phonegap.plugin.statusbar": "1.1.0",
+    "org.apache.cordova.inappbrowser": "0.4.0"
+}
+// BOTTOM OF METADATA
+});
